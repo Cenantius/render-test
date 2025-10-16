e1568dc1ca542d2632a2001a5c722b3d9f3f24b3
Iisakki Marjomäki <cycklingben@gmail.com>
2025-10-16 15:38:57 +0300

    uibuild

--- index.js
+++ index.js
@@ -61,50 +61,11 @@ let persons = [
   { id: "24", name: "Kaisa Peltonen",     number: "040-123024" },
   { id: "25", name: "Lauri Vainio",       number: "040-123025" }
 ]
-// ALKAA TÄSTÄ
-
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-// const password = process.argv[2]
-
-// const Person = mongoose.model('Person', personSchema)
-
-if (process.argv.length === 3) {
-
-    Person.find({}).then(result => {
-        console.log('phonebook:')
-        result.forEach(person => {
-            console.log(person.name + ' ' + person.number)
-        })
-        mongoose.connection.close()
-    })
-}
-
-if (process.argv.length === 5) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4],
-    })
-
-    person.save().then(result => {
-    console.log('added ' + person.name + ' number ' + person.number + ' to phonebook')
-    mongoose.connection.close()
-    })
-}
-// LOPPUU TÄHÄN
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -142,15 +103,15 @@ app.post('/api/persons', (request, response) => {
         })
     }
     
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
         id: randId.toString(),
-    }
+    })
       
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 const unknownEndpoint = (request, response) => {
